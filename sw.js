18c49521fb3fbaf8fcdac6aaf5213123fd352896
Muniz-2026 <tito@munizcontracting.com>
2026-09-04 17:46:57 -0500

    v6.4 - PO Updates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* MUÑIZ Pedidos - service worker: offline app shell + instant load (fully self-contained) */
-const CACHE = "muniz-pedidos-v30";
+const CACHE = "muniz-pedidos-v31";
 const SHELL = [
   "./","./index.html","./app.js","./styles.css","./manifest.json","./config.js","./catalog_ace.json","./catalog_cmc.json","./fotos_ace.json","./fotos_cmc.json","./catalog_rss.json","./fotos_rss.json","./catalog_whitecap.json","./fotos_whitecap.json",
   "./icon-192.png","./icon-512.png","./apple-touch-icon.png"
